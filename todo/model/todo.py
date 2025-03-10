41fac94de93f871505617cd36856b79224e70139
davidd garcia <marincontaduria@gmail.com>
2025-03-10 13:55:39 -0500

    crear segunda clase y sus metodos

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -22,3 +22,16 @@ class Todo:
 
 
 class TodoBook:
+    def __init__(self):
+        self.todos={}
+
+    def add_todo(self,title:str, description:str)->int:
+
+
+    def pending_todos(self):
+        pass
+
+    def completed_todos(self):
+        pass
+    def tags_todo_count(self)-> list :
+        pass
